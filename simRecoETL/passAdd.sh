274a4fbfcb258ca14c99688f522f91ee59603fd6
Ben Tannenwald <benjamin.tannenwald@cern.ch>
2019-02-06 19:35:39 -0600

    update passAdd script for use with new ntuples

--- simRecoETL/passAdd.sh
+++ simRecoETL/passAdd.sh
@@ -1,4 +1,4 @@
-hadd minBias14TeV_v0_$1/minBias14TeV_v0_$1.root /eos/uscms/store/user/benjtann/minBias14TeV_v0_$1/minBias14TeV_v0_$1/minBias14TeV_*.root
-hadd singleMuon_v0_$1/singleMuon_v0_$1.root /eos/uscms/store/user/benjtann/singleMuon_v0_$1/singleMuon_v0_$1/singleMuon_*.root
-hadd singlePion_v0_$1/singlePion_v0_$1.root /eos/uscms/store/user/benjtann/singlePion_v0_$1/singlePion_v0_$1/singlePion_*.root
+hadd nuGun200PU_v1_$1/nuGun200PU_v1_$1.root /eos/uscms/store/user/benjtann/nuGun200PU_v1_$1/nuGun200PU_v1_$1/nuGun200PU_*.root
+hadd singleMuon_v1_$1/singleMuon_v1_$1.root /eos/uscms/store/user/benjtann/singleMuon_v1_$1/singleMuon_v1_$1/singleMuon_*.root
+hadd singlePion_v1_$1/singlePion_v1_$1.root /eos/uscms/store/user/benjtann/singlePion_v1_$1/singlePion_v1_$1/singlePion_*.root
 

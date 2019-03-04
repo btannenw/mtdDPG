3631fd8ba107d2666ca9d5d4163e514c7e8a78f5
Ben Tannenwald <benjamin.tannenwald@cern.ch>
2019-03-04 13:51:33 -0600

    added minBias 0PU options

--- simRecoETL/passSub_splitNuGun.sh
+++ simRecoETL/passSub_splitNuGun.sh
@@ -1,26 +1,27 @@
 #!/bin/csh
 
-setenv SPLIT 10
+setenv SPLIT 7
 
 foreach x (`seq 0 $SPLIT 49`)
-    if ($x == 0) then
 	#echo `expr $x + $SPLIT - 1`
-	python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events `expr $x + $SPLIT - 1`
+    if ($x == 0) then
+	# *** A) Neutrino Gun, 200 PU
+	#python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events `expr $x + $SPLIT - 1`
+	# *** B) MinBias, 0 PU
+	python submitSampleToCondor.py --outputDir minBias0PU_v1_$1 --inputTXTfile fileLists/minBias0PU_v1.txt --layout barzflat --events `expr $x + $SPLIT - 1`
     else if ($x > 0) then
-	#echo `expr $SPLIT - 1`, $x
-	python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events `expr $SPLIT - 1` --firstEvent $x
+	# *** A) Neutrino Gun, 200 PU
+	#python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events `expr $SPLIT - 1` --firstEvent $x
+	# *** B) MinBias, 0 PU
+	python submitSampleToCondor.py --outputDir minBias0PU_v1_$1 --inputTXTfile fileLists/minBias0PU_v1.txt --layout barzflat --events `expr $SPLIT - 1` --firstEvent $x
     endif
 
     sleep 10
 end
 
-#python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events 24
+#python submitSampleToCondor.py --outputDir singleMuon_v1_$1 --inputTXTfile fileLists/singleMuon_v1.txt --layout barzflat
 #sleep 10
-#python submitSampleToCondor.py --outputDir nuGun200PU_v1_$1 --inputTXTfile fileLists/nuGun200PU_v1.txt --layout barzflat --events 25 --firstEvent 25
-#sleep 10
-python submitSampleToCondor.py --outputDir singleMuon_v1_$1 --inputTXTfile fileLists/singleMuon_v1.txt --layout barzflat
-sleep 10
-python submitSampleToCondor.py --outputDir singlePion_v1_$1 --inputTXTfile fileLists/singlePion_v1.txt --layout barzflat
+#python submitSampleToCondor.py --outputDir singlePion_v1_$1 --inputTXTfile fileLists/singlePion_v1.txt --layout barzflat
 
   
 
--- simRecoETL/quickFilelistMaker.sh
+++ simRecoETL/quickFilelistMaker.sh
@@ -3,7 +3,9 @@ rm fileLists/*_v1.txt -v
 ls /eos/uscms/store/user/benjtann/RelValNuGun/runHitsRelValNuGunv10_v10/190206_162648/0000/DumpHits_*.root > fileLists/nuGun200PU_v1.txt
 ls /eos/uscms/store/user/benjtann/RelValSinglePiFlatPt_0p7to10_pythia8_cfi/runHitsRelValSinglePiFlatPt0p7to10pythia8cfiv10_v10/190206_162656/0000/DumpHits_*.root > fileLists/singlePion_v1.txt
 ls /eos/uscms/store/user/benjtann/RelValSingleMuFlatPt_0p7to10_pythia8/runHitsRelValSingleMuFlatPt0p7to10pythia8v10_v10/190206_162703/0000/DumpHits_*.root > fileLists/singleMuon_v1.txt
+ls /eos/uscms/store/user/benjtann/MinBias_TuneCP5_14TeV-pythia8/runHitsMinBiasTuneCP514TeV-pythia8-v10r2_v10_r2/190301_162542/0000/DumpHits_*.root > fileLists/minBias0PU_v1.txt
 
 sed -i 's/\/eos\/uscms/root:\/\/cmseos.fnal.gov\//g' fileLists/nuGun200PU_v1.txt
+sed -i 's/\/eos\/uscms/root:\/\/cmseos.fnal.gov\//g' fileLists/minBias0PU_v1.txt
 sed -i 's/\/eos\/uscms/root:\/\/cmseos.fnal.gov\//g' fileLists/singleMuon_v1.txt
 sed -i 's/\/eos\/uscms/root:\/\/cmseos.fnal.gov\//g' fileLists/singlePion_v1.txt
